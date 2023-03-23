80266468619c3fd787d264b1f70d74b49f746ef6
Alan Wu <alanjialongwu@gmail.com>
2023-03-23 17:23:02 +1100

    synchronizing database for development

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,13 @@
 const {Events} = require('discord.js');
+const game = require('../models/game');
+const {Added, Game} = require('../models/models');
 
-module.exportss = {
+module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client){
+        Added.sync({ force: true });
+        Game.sync({ force: true });
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
 };
